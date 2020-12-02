ef1e083c3c0880c82859a7cfc86ece514f23787c
wi19b027 <wi19b027@technikum-wien.at>
2020-12-02 13:00:46 +0100

    commit

---
+++ neues Projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neues_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
